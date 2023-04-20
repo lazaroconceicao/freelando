45b5717cb4877210eb39de223c41c372b2dab44f
lazaroconceicao <joselazaroc@gmail.com>
2023-04-20 16:40:24 -0300

    Creating typography and text field components

---
+++ src/componentes/CampoDeTexto/CampoDeTexto.jsx
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled"
+
+const LabelEstlizada = styled.label`
+    display: block;
+    width: 100%;
+    box-sizing: border-box;   
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    
+`
+const InputlEstlizado = styled.input`
+    display: block;
+    width: 100%;
+    margin-top: ${props => props.theme.espacamentos.xs};
+    background: ${props => props.theme.cores.branco};
+    border: 1px solid;
+    border-color: ${props => props.theme.cores.neutras.a};
+    border-radius: ${props => props.theme.espacamentos.s};
+    height: 40px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+`
+
+export const CampoDeTexto = ( { titulo }) => {
+    return (<LabelEstlizada>
+        {titulo}
+        <InputlEstlizado />
+    </LabelEstlizada>)
+}
